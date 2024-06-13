20030f4691b83702546e7ee861b9b81d32b8bb17
josie <josephine.lucyrita@gmail.com>
2024-06-13 12:40:35 +0100

    adds UserTab component

---
+++ src/Components/UserTab/index.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../context/User";
+
+const UserTab = ({ user }) => {
+  const { setUser } = useContext(UserContext);
+
+  return (
+    <div key={user.username} className="user-tab">
+      <p>{user.username}</p>
+      <img
+        className="user-tab__icon"
+        src={user.avatar_url}
+        alt={user.username}
+      />
+      <Link to="/">
+        <button
+          className="button"
+          onClick={() => {
+            setUser(user);
+          }}
+        >
+          {`Login as ${user.username}`}
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default UserTab;
